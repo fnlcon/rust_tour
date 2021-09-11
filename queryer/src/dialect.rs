201cd96b9178478c6d09fbf8846a3c8f00e3ae3a
fnlcon <fnlcon@outlook.com>
2021-09-11 13:20:42 +0800

    add test.txt

--- queryer/src/dialect.rs
+++ queryer/src/dialect.rs
@@ -7,7 +7,7 @@ pub struct TyrDialect;
 
 impl Dialect for TyrDialect {
     fn is_identifier_start(&self, ch: char) -> bool {
-        todo!()
+        ('a'..='z').contains(&ch)
     }
 
     fn is_identifier_part(&self, ch: char) -> bool {
