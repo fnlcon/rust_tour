2a69e037e3928462cdeabc63a97eb36911d065cc
fnlcon <fnlcon@outlook.com>
2021-09-13 22:43:14 +0800

    request

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::error::Error;
+use reqwest::Client;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+    let client = Client::builder().build()?;
+    let request = client.get("http://www.bilibili.com").build()?;
+    let response = client.execute(request).await?;
+    print!("{}", response.text().await?);
+
+    Ok(())
 }
